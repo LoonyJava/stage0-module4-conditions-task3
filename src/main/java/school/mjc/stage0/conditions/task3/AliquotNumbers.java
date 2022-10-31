e13abe4246989e76011d745a3e8780b2acae3419
LoonyJava <loony.java@gmail.com>
2022-10-31 17:05:39 +0500

    Task #3. Multiple Conditions

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,10 +2,8 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
-        if (first != second) {
+        if (first % second != 0 && second % first == 0) {
             System.out.println("Aliquot");
-        } else if (first % second == 0 || second % first == 0) {
-            System.out.println("Not aliquot");
         } else System.out.println("Not aliquot");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -4,16 +4,20 @@ public class SalaryCalculator {
     public void calculateSalary(int salary) {
         double result = 0;
         double taxes = 0;
-        if (salary <= 0) {
+        if (salary < 0) {
             System.out.println("wrong input!");
         } else if (salary <= 10000) {
             taxes = 0.15;
+            result = salary - (salary * taxes);
+            System.out.println(result);
         } else if (10000 < salary && salary <= 20000) {
             taxes = 0.18;
+            result = salary - (salary * taxes);
+            System.out.println(result);
         } else if (salary > 20000) {
             taxes = 0.2;
+            result = salary - (salary * taxes);
+            System.out.println(result);
         }
-        result = salary - (salary * taxes);
-        System.out.println(result);
     }
 }
